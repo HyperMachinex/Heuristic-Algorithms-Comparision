80b8b6767af9bdcf3c30e9192bb305b89c6349a6
Hyper <armagan.trs@protonmail.ch>
2024-10-09 20:06:38 +0300

    other function tests are added.

--- Grey-Wolf-Optimization/gwo-perm.py
+++ Grey-Wolf-Optimization/gwo-perm.py
@@ -1,9 +1,9 @@
 from GWO import GWO
 import functions as functions
-obj_func = functions.selectFunction(6)
+obj_func = functions.selectFunction(3)
 
-lower_bound = -500
-upper_bound = 500
+lower_bound = -30
+upper_bound = 30
 dimension = 30
 population = [100, 250, 500, 1000]
 iteration = [100, 250, 500, 1000]
@@ -15,7 +15,7 @@ def do_test():
             for k in iteration:
                 for l in a_values:
                     print(f"Grey Wolf Algorithm - Population: {i} - Iteration: {k} - Value-a: {l}, Test Number: {j+1}")
-                    gwo(i, k, j+1, 'schwefel', l)
+                    gwo(i, k, j+1, 'perm', l)
 
 def gwo(pop_size, iter, rep, folder, a):
     GWO(obj_func, lower_bound, upper_bound, dimension, SearchAgents_no = pop_size, Max_iter = iter, repeat = rep, folder = folder, value_a = a)
